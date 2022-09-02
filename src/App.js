e8ef59d0b883ea93db49df5c59aee9d7bc0df6ac
Ian-Saia <104043117+Ian-Saia@users.noreply.github.com>
2022-09-01 22:35:36 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () =>{
+  return (
+  <>
+    <header>Soy Header</header>
+    <main>Soy Main</main>
+    <footer>Soy Footer</footer>
+  </>
+  )
+}
+
+export default App
+
